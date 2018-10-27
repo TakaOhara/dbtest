28de464c0932163434790c25b919404cb3187379
Famfam <famfam.lancers@gmail.com>
2018-10-28 00:09:45 +0900

    コメント追加

--- src/test/java/com/example/dbtest/DbtestApplicationTests.java
+++ src/test/java/com/example/dbtest/DbtestApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+// https://spring.io/guides/gs/testing-web/
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DbtestApplicationTests {
